f898a0730f5b4d76a6ba29866f8a4daf0ef93495
Atakan Durmaz <durmazatakan99@hotmail.com>
2020-01-23 16:19:40 +0300

    Update ServoSpeedControl.ino

--- ServoSpeedControl/ServoSpeedControl.ino
+++ ServoSpeedControl/ServoSpeedControl.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 //int pos=0;
 //int val;
